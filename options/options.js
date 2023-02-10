dfa399560ace078d9c389b13818caeb34aeb788b
Samuel Mpwanyi <samuel.m@crowdbotics.com>
2023-02-10 23:17:11 +0300

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "sam-chat-dev-62733",
   url: "https://sam-chat-dev-62733.botics.co",
   api: "https://sam-chat-dev-62733.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
